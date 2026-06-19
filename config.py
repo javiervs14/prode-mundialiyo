9c3373cdbbb9336998c8cc7ff50f210813972ba3
Diego Olivera <javier.vs@live.com>
2026-06-19 15:13:55 -0300

    Fix PostgreSQL SSL connection error with sslmode=disable

--- config.py
+++ config.py
@@ -8,6 +8,13 @@ class Config:
     _database_url = os.getenv("DATABASE_URL", "sqlite:///prode.db")
     if _database_url and _database_url.startswith("postgres://"):
         _database_url = _database_url.replace("postgres://", "postgresql://", 1)
+    if _database_url and "postgresql" in _database_url:
+        import re
+        _database_url = re.sub(r'[\?&]sslmode=[^&]*', '', _database_url)
+        if "?" in _database_url:
+            _database_url += "&sslmode=disable"
+        else:
+            _database_url += "?sslmode=disable"
     SQLALCHEMY_DATABASE_URI = _database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY", "")
